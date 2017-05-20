fdf421846d3b3c34bfc51a69cfcacfa1f9c1e3f3
Arsen <daa_91@mail.ru>
2017-05-20 11:21:13 +0300

    Delete conf.py

--- conf.py
+++
@@ -1,6 +0,0 @@
-#TODO do debug/stage selctor
-token = ""
-channel_name = '@channel_name' #dev channel
-channel_alarm_name = '@alarm_channel_name' #notification channel
-imgDir = 'botimg'
-sentImgDir = 'botimg/sent' #abs path need
